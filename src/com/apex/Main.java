c43628f541194d64a392885aee9cddfcc4f1c0b5
Yash <Yashp1196@gmail.com>
2020-05-05 00:13:26 -0700

    first commit...

---
+++ src/com/apex/Main.java
@@ -0,0 +1,12 @@
+package com.apex;
+
+public class Main {
+	public static void main(String[] args) {
+		String name = "yash";
+		if(name.equals("yash")) {
+			System.out.println("Welcome Sir");
+		}else {
+			System.out.println("Please try again!!");
+		}
+	}
+}
